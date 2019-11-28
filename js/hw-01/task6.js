99efe3fef1f545e18e9ac024e173b0327eab1313
a-sapon <sapon.anya@gmail.com>
2019-11-28 12:50:47 +0200

    added to task-06

--- js/hw-01/task6.js
+++ js/hw-01/task6.js
@@ -2,14 +2,26 @@ function task6() {
   let input;
   let total = 0;
 
-  do {
-    input = Number(prompt('Please enter a number.'));
-    if (!isNaN(input)) {
-      total += input;
-    } else {
-      alert('Wrong input! Please enter a NUMBER');
-    }
-  } while (input !== 0);
+  // do {
+  //   input = Number(prompt('Please enter a number.'));
+  //   if (!isNaN(input)) {
+  //     total += input;
+  //   } else {
+  //     alert('Wrong input! Please enter a NUMBER');
+  //   }
+  // } while (input !== 0);
 
+  // alert(`Total amount of your inputs is ${total}`);
+
+  do {
+    input = prompt('Please enter a number.');
+    if(input === null) {
+      break;
+    } else if(!isNaN(input)) {
+      input = Number(input);
+      total += input;
+    }
+  } while (true);
+  
   alert(`Total amount of your inputs is ${total}`);
 }
