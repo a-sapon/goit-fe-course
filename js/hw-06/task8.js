0c42ea839c3a6ad8c9efa315d697f1b44688cddf
a-sapon <sapon.anya@gmail.com>
2019-12-17 10:03:16 +0200

    fixed task8

--- js/hw-06/task8.js
+++ js/hw-06/task8.js
@@ -1,7 +1,9 @@
 import users from './users.js';
 
 const getUsersWithFriend = (users, friendName) => {
-  const friend = users.filter(user => user.friends.includes(friendName));
+  const friend = users
+    .filter(user => user.friends.includes(friendName))
+    .map(user => user.name);
   return friend;
 };
 
